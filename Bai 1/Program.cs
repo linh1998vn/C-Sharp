4f6ea66b4bb2421b2cc3da8a590aa94405a57955
linh1998vn <81789760+linh1998vn@users.noreply.github.com>
2022-03-22 12:02:27 +0700

    Bai Tap

---
+++ Bai 1/Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+long result = 1;
+Console.Write("Nhap so nguyen can tinh: ");
+
+n = Convert.ToInt32(Console.ReadLine());
+
+for (var i = 1; i <= n; i++)
+{
+    result *= i;
+}
+
+Console.WriteLine($"giai thua cua {n} la {result}");
---
+++ Bai 2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, max = 0;
+
+do
+{
+    Console.WriteLine("Nhap so n: ");
+    n = Convert.ToInt32(Console.ReadLine());
+
+    if (max < n)
+        max = n;
+} while (n != 0);
+
+Console.WriteLine($"So lon nhat la: {max}");
