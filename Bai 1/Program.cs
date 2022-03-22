93948e62d75b6bef3ebc0f9d4985e8df8b1cde4b
linh1998vn <81789760+linh1998vn@users.noreply.github.com>
2022-03-22 11:51:09 +0700

    Bai 1

---
+++ Bai 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            long result = 1;
+            Console.Write("Nhap so nguyen can tinh: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine($"giai thua cua {n} la {result}");
+        }
+    }
+}
