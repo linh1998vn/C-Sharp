242a70dbe72f3be50dc701b23180031bab02e05e
linh1998vn <81789760+linh1998vn@users.noreply.github.com>
2022-03-22 20:16:41 +0700

    Thi

---
+++ Thi/GeometryExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    class Cylinder
+    {
+        static string Process(double a, double b)
+        {
+            double BaseArea, LateralArea, TotalArea, Volumn;
+            BaseArea = a * a * Math.PI;
+            LateralArea = 2 * Math.PI * a * b;
+            TotalArea = 2 * Math.PI * a * (a + b);
+            Volumn = Math.PI * a * a * b;
+            return "Base: " + BaseArea + " | Laterial: " + LateralArea + " | Total: " + TotalArea + " | Volume: " + Volumn;
+        }
+        static void Main(string[] args)
+        {
+            double r, h;
+            Console.Write("Radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Cylinder Characteristics\nRadius: " + r + ", Height: " + h);
+            Console.WriteLine(Process(r, h));
+        }
+    }
+}
